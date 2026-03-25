6c4402e26080023fbc8865c6268783d87657292e
marcosarus <marcos.aurelio.rus@gmail.com>
2026-03-25 20:13:05 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        System.out.println("Digite o primeiro número:");
+        double a = sc.nextDouble();
+
+        System.out.println("Digite o segundo número:");
+        double b = sc.nextDouble();
+
+        System.out.println("Digite a operação (+ - * /):");
+        char operacao = sc.next().charAt(0);
+
+        double resultado = 0;
+
+        if(operacao == '+') {
+            resultado = a + b;
+        } else if(operacao == '-') {
+            resultado = a - b;
+        } else if(operacao == '*') {
+            resultado = a * b;
+        } else if(operacao == '/') {
+            resultado = a / b;
+        } else {
+            System.out.println("Essa operação não é válida para esse sistema");
+            return;
+        }
+
+        System.out.printf(Locale.US, "O Resultado desejado é: %.2f\n", resultado);
+
+        sc.close();
+    }
+}
